6cc93dc1b6b7165a9a90b5ae773427d8cc11e731
imhta <imhtapm@gmail.com>
2020-01-28 01:18:35 +0530

    :hammer: use hooks over jsx element based static query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,18 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
-import { StaticQuery } from "gatsby"
-const IndexPage = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        gcms {
-          configs {
-            type
-          }
-        }
-      }
-    `}
-    render={data => (
-      <Layout>
-        <SEO title="Home" />
-        <h1>Hello, I'm Pon Muthu Selvam</h1>{" "}
-        <h1> a web developer {JSON.stringify(data.gcms.configs)}</h1>
-      </Layout>
-    )}
-  />
-)
+import configUtils from "../utils/configUtils"
+import { useSiteConfigs } from "../hooks/use-site-configs"
+const IndexPage = () => {
+  const configs = useSiteConfigs()
+  return(
+    <Layout>
+      <SEO title="Home" />
+      <h1>{configUtils(configs).getValueByType("MAIN_TITLE")}</h1>
+      <h1>{configUtils(configs).getValueByType("MAIN_SUBTITLE")}</h1>
+    </Layout>
+  );
+}
 
 export default IndexPage
