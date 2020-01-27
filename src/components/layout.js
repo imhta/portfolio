cf3df1cc5bc434a96f516989b374325fb89f4ef9
imhta <imhtapm@gmail.com>
2020-01-28 01:17:47 +0530

    :fire: remove unnecessary code or no use code

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,38 +7,19 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
 
   return (
     <>
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, 
           {` `}
           <a href="https://ponmuthuselvam.me">ponmuthuselvam.me</a>
         </footer>
-      </div>
     </>
   )
 }
