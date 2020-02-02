b245e741420b27de2db26bdf7f3e75af1267e241
imhta <imhtapm@gmail.com>
2020-02-03 00:05:35 +0530

    :hammer: seo basic metatag change

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `ponmuthuselvam's portfolio`,
+    description: `Web developer | Engineer who craft applications with love 💖`,
+    author: `@ponmuthuselvam`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `ponmuthuselvam's portfolio`,
+        short_name: `portfolio`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
